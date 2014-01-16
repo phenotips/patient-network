4d1c2159c9f17bf0df7223d007dc2f8db955d0bf
Orion Buske <orion.buske@gmail.com>
2014-01-16 18:02:53 -0500

    Jonathan's mutual information feature similarity scorer is now functioning without crashing, but tests are not yet written and it doesn't use real frequencies.

--- similarity-search/src/main/java/org/phenotips/similarity/internal/SolrSimilarPatientsFinder.java
+++ similarity-search/src/main/java/org/phenotips/similarity/internal/SolrSimilarPatientsFinder.java
@@ -76,7 +76,7 @@ public class SolrSimilarPatientsFinder implements SimilarPatientsFinder, Initial
 
     /** Allows to make a secure pair of patients. */
     @Inject
-    @Named("jonathan")
+    @Named("mi")
     private PatientSimilarityViewFactory factory;
 
     /** The Solr server instance used. */
