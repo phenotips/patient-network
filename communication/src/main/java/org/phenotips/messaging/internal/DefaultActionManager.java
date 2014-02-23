ce3cf532b39b89495119365b4da30a6ef23d4995
Sergiu Dumitriu <sergiu@phenotips.org>
2014-02-23 02:32:05 -0500

    [misc] Better sender name displayed in the contact email

--- communication/src/main/java/org/phenotips/messaging/internal/DefaultActionManager.java
+++ communication/src/main/java/org/phenotips/messaging/internal/DefaultActionManager.java
@@ -71,8 +71,8 @@ public class DefaultActionManager implements ActionManager
             options.put("matchCaseId", connection.getTargetPatient().getDocument().getName());
             options.put("matchCaseAccessLink",
                 xwiki.getExternalURL("data.GrantMatchAccess", "view", "id=" + connection.getId(), context));
-            mailsender.sendMailFromTemplate("PhenoTips.MatchContact", "noreply@phenomecentral.org", to, null, null, "",
-                options, context);
+            mailsender.sendMailFromTemplate("PhenoTips.MatchContact", "PhenomeCentral <noreply@phenomecentral.org>", to,
+                null, null, "", options, context);
             return 0;
         } catch (Exception ex) {
             this.logger.error("Failed to send email: [{}]", ex.getMessage(), ex);
