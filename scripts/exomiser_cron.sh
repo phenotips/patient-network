57e9a39bd5d93ef890b99b750da778fa8c74a011
Orion Buske <orion.buske@gmail.com>
2016-05-19 17:35:08 -0400

    [misc] reduced Exomiser cron update window

--- scripts/exomiser_cron.sh
+++ scripts/exomiser_cron.sh
@@ -1,1 +1,1 @@
-python ~/cron/exomiser_cron.py --since=`date --date="1 day ago" +%Y-%m-%d` /var/lib/phenotips /var/lib/exomiser/exomiser-cli-6.0.0/exomiser-cli-6.0.0.jar /var/lib/exomiser/exomiser.credentials &> ~/cron/exomiser_cron_logs/`date +%Y-%m-%d.%H%M%S`.log
+python ~/cron/exomiser_cron.py --since=`date --date="1 hour ago" +%Y-%m-%d` /var/lib/phenotips /var/lib/exomiser/exomiser-cli-6.0.0/exomiser-cli-6.0.0.jar /var/lib/exomiser/exomiser.credentials &> ~/cron/exomiser_cron_logs/`date +%Y-%m-%d.%H%M%S`.log
