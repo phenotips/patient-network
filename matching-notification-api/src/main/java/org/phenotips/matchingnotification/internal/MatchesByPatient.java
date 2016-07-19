09ccd174ea819417702b5ed43cc483b4af70e169
ItaiGershtansky <itaig.phenotips@gmail.com>
2016-07-19 16:44:40 -0400

    Added MatchesByPatient and test.

---
+++ matching-notification-api/src/main/java/org/phenotips/matchingnotification/internal/MatchesByPatient.java
@@ -0,0 +1,195 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+package org.phenotips.matchingnotification.internal;
+
+import org.phenotips.matchingnotification.match.PatientMatch;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @version $Id$
+ */
+public class MatchesByPatient
+{
+    /*
+     * Main key of internalMap is id of local patient in match. Secondary key is id of the other patient.
+     * If both patients are local, a match will be inserted twice.
+     * Examples with match expressed as
+     *    (reference patient id, server patient id, matched patient id, matched server id)
+     *
+     * 1. Adding m1=("p1", -, "p2", -). m1 will be found in both sets
+     *    internalMap.get("p1").get("p2") and
+     *    internalMap.get("p2").get("p1").
+     * 2. Adding m2=("p2", -, "p1", -) after m1 was added.
+     *    internalMap.get("p1").get("p2") returns set s1 containing m1 and m2.
+     *    internalMap.get("p2").get("p1") returns set s2 containing m1 and m2.
+     * 3. Adding m3=("p3","server1","p1",-). m3 will be found in the set
+     *    internalMap.get("p1").get("p3") (and no other sets).
+     * 4. Adding m4=("p1",-,"p3","server1"). Match will be found in the same set as 3 and not other sets.
+     * 5. Adding m5=("p1",-,"p2","server1"). Match will be found in
+     *    internalMap.get("p1").get("p2") in addition to m1 and m2.
+     */
+    private Map<String, Map<String, Set<PatientMatch>>> internalMap;
+
+    /**
+     * Creates an empty collection.
+     */
+    public MatchesByPatient() {
+        this.internalMap = new TreeMap<>();
+    }
+
+    /**
+     * Creates a collection with all given matches.
+     *
+     * @param matches a collection of PatientMatch-es
+     */
+    public MatchesByPatient(List<PatientMatch> matches) {
+        this();
+        for (PatientMatch match : matches) {
+            this.add(match);
+        }
+    }
+
+    /**
+     * @return number of matches in collection
+     */
+    public int size()
+    {
+        Set<PatientMatch> matches = new HashSet<PatientMatch>();
+        for (Map<String, Set<PatientMatch>> matchesForLocalPatient : this.internalMap.values()) {
+            for (Set<PatientMatch> set : matchesForLocalPatient.values()) {
+                matches.addAll(set);
+            }
+        }
+
+        return matches.size();
+    }
+
+    /**
+     * Adds a PatientMatch to the collection.
+     *
+     * @param match to add
+     */
+    public void add(PatientMatch match)
+    {
+        // This will insert a match once or twice, because at least one of the patients is local
+        if (match.getReferenceServerId() == null) {
+            this.add(match.getReferencePatientId(), match.getMatchedPatientId(), match);
+        }
+        if (match.getMatchedServerId() == null) {
+            this.add(match.getMatchedPatientId(), match.getReferencePatientId(), match);
+        }
+    }
+
+    /**
+     * @return a collection of all ids of local patients.
+     */
+    public Collection<String> getLocalPatientIds()
+    {
+        return this.internalMap.keySet();
+    }
+
+    /**
+     * Returns all matches for a local patient id. This will return matches where the patient is both
+     * reference patient and matched patient.
+     *
+     * @param localPatientId id of local patient
+     * @return collection of all matches for {@code localPatientId}
+     */
+    public Collection<PatientMatch> getMatchesForLocalPatientId(String localPatientId)
+    {
+        List<PatientMatch> list = new LinkedList<>();
+
+        Map<String, Set<PatientMatch>> matchesForPatient = this.internalMap.get(localPatientId);
+        for (Set<PatientMatch> set : matchesForPatient.values()) {
+            list.addAll(set);
+        }
+        return list;
+    }
+
+    /**
+     * Returns an equivalent match, or null if doesn't exist.
+     * A match m1 is equivalent to a match m2 if all the following are true:
+     *    m1.referencePatientId = m2.matchedPatientId
+     *    m1.referenceServerId  = m2.matchedServerId
+     *    m2.matchedPatientId   = m1.referencePatientId
+     *    m2.matchedPatientId   = m1.referenceServerId
+     *
+     * @param match to find an equivalent to
+     * @return an equivalent PatientMatch or null if doesn't exist
+     */
+    public PatientMatch getEquivalentMatch(PatientMatch match)
+    {
+        Set<PatientMatch> setToSearch = null;
+        if (match.getReferenceServerId() == null) {
+            setToSearch = this.getSet(match.getReferencePatientId(), match.getMatchedPatientId());
+        } else {
+            setToSearch = this.getSet(match.getMatchedPatientId(), match.getReferencePatientId());
+        }
+
+        if (setToSearch == null) {
+            return null;
+        }
+
+        for (PatientMatch m : setToSearch) {
+            if (StringUtils.equals(m.getReferencePatientId(), match.getMatchedPatientId())
+                && StringUtils.equals(m.getReferenceServerId(), match.getMatchedServerId())
+                && StringUtils.equals(m.getMatchedPatientId(), match.getReferencePatientId())
+                && StringUtils.equals(m.getMatchedServerId(), match.getReferenceServerId())) {
+                return m;
+            }
+        }
+
+        return null;
+    }
+
+    private void add(String localPatientId, String otherPatientId, PatientMatch match)
+    {
+        Map<String, Set<PatientMatch>> map = this.internalMap.get(localPatientId);
+        if (map == null) {
+            map = new TreeMap<>();
+            this.internalMap.put(localPatientId, map);
+        }
+
+        Set<PatientMatch> set = map.get(otherPatientId);
+        if (set == null) {
+            set = new HashSet<>();
+            map.put(otherPatientId, set);
+        }
+
+        set.add(match);
+    }
+
+    private Set<PatientMatch> getSet(String localPatientId, String otherPatientId)
+    {
+        Map<String, Set<PatientMatch>> map = this.internalMap.get(localPatientId);
+        if (map == null) {
+            return null;
+        }
+
+        return map.get(otherPatientId);
+    }
+}
---
+++ matching-notification-api/src/test/java/org/phenotips/matchingnotification/internal/test/MatchesByPatientTest.java
@@ -0,0 +1,190 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+package org.phenotips.matchingnotification.internal.test;
+
+import org.phenotips.matchingnotification.internal.MatchesByPatient;
+import org.phenotips.matchingnotification.match.PatientMatch;
+
+import java.util.Collection;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * @version $Id$
+ */
+public class MatchesByPatientTest
+{
+    private MatchesByPatient mbp;
+
+    private PatientMatch m1_3, m1_4, m1_5server1, m1server3_4, m1server4_4, m1server5_4,
+        m2_1, m2_3, m2_3server1,
+        m3_1, m3server1_2,
+        m4_1, m4_1server3, m4_2,
+        m5server1_1, m5server1_2;
+
+    @Before
+    public void setup()
+    {
+        m1_3 = newMatch("P1", null, "P3", null);
+        m1_4 = newMatch("P1", null, "P4", null);
+        m1_5server1 = newMatch("P1", null, "P5", "server1");
+        m1server3_4 = newMatch("P1", "server3", "P4", null);
+        m1server4_4 = newMatch("P1", "server4", "P4", null);
+        m1server5_4 = newMatch("P1", "server5", "P4", null);
+
+        m2_1 = newMatch("P2", null, "P1", null);
+        m2_3 = newMatch("P2", null, "P3", null);
+        m2_3server1 = newMatch("P2", null, "P3", "server1");
+
+        m3_1 = newMatch("P3", null, "P1", null);
+        m3server1_2 = newMatch("P3", "server1", "P2", null);
+
+        m4_1 = newMatch("P4", null, "P1", null);
+        m4_1server3 = newMatch("P4", null, "P1", "server3");
+        m4_2 = newMatch("P4", null, "P2", null);
+
+        m5server1_1 = newMatch("P5", "server1", "P1", null);
+        m5server1_2 = newMatch("P5", "server1", "P2", null);
+
+        this.mbp = new MatchesByPatient();
+        mbp.add(m1_3);
+        mbp.add(m1_4);
+        mbp.add(m1_5server1);
+        mbp.add(m1server3_4);
+        mbp.add(m1server4_4);
+        mbp.add(m1server5_4);
+        mbp.add(m2_1);
+        mbp.add(m2_3);
+        mbp.add(m2_3server1);
+        mbp.add(m3_1);
+        mbp.add(m3server1_2);
+        mbp.add(m4_1);
+        mbp.add(m4_1server3);
+        mbp.add(m4_2);
+        mbp.add(m5server1_1);
+        mbp.add(m5server1_2);
+    }
+
+    @Test
+    public void testSize()
+    {
+        Assert.assertEquals(16, this.mbp.size());
+    }
+
+    @Test
+    public void testPatientIds()
+    {
+        Collection<String> patientIds = this.mbp.getLocalPatientIds();
+        Assert.assertEquals(patientIds.size(), 4);
+        Assert.assertTrue(patientIds.contains("P1"));
+        Assert.assertTrue(patientIds.contains("P2"));
+        Assert.assertTrue(patientIds.contains("P3"));
+        Assert.assertTrue(patientIds.contains("P4"));
+    }
+
+    @Test
+    public void testGetMatches1()
+    {
+        Collection<PatientMatch> matches = mbp.getMatchesForLocalPatientId("P1");
+        Assert.assertEquals(7, matches.size());
+        Assert.assertTrue(matches.contains(m1_3));
+        Assert.assertTrue(matches.contains(m1_4));
+        Assert.assertTrue(matches.contains(m1_5server1));
+        Assert.assertTrue(matches.contains(m2_1));
+        Assert.assertTrue(matches.contains(m3_1));
+        Assert.assertTrue(matches.contains(m4_1));
+        Assert.assertTrue(matches.contains(m5server1_1));
+    }
+
+    @Test
+    public void testGetMatches2()
+    {
+        Collection<PatientMatch> matches = mbp.getMatchesForLocalPatientId("P2");
+        Assert.assertEquals(6, matches.size());
+        Assert.assertTrue(matches.contains(m2_1));
+        Assert.assertTrue(matches.contains(m2_3));
+        Assert.assertTrue(matches.contains(m2_3server1));
+        Assert.assertTrue(matches.contains(m3server1_2));
+        Assert.assertTrue(matches.contains(m4_2));
+        Assert.assertTrue(matches.contains(m5server1_2));
+    }
+
+    @Test
+    public void testGetMatches3()
+    {
+        Collection<PatientMatch> matches = mbp.getMatchesForLocalPatientId("P3");
+        Assert.assertEquals(3, matches.size());
+        Assert.assertTrue(matches.contains(m1_3));
+        Assert.assertTrue(matches.contains(m2_3));
+        Assert.assertTrue(matches.contains(m3_1));
+    }
+
+    @Test
+    public void testGetMatches4()
+    {
+        Collection<PatientMatch> matches = mbp.getMatchesForLocalPatientId("P4");
+        Assert.assertEquals(7, matches.size());
+        Assert.assertTrue(matches.contains(m1_4));
+        Assert.assertTrue(matches.contains(m1server3_4));
+        Assert.assertTrue(matches.contains(m1server4_4));
+        Assert.assertTrue(matches.contains(m1server5_4));
+        Assert.assertTrue(matches.contains(m4_1));
+        Assert.assertTrue(matches.contains(m4_1server3));
+        Assert.assertTrue(matches.contains(m4_2));
+    }
+
+    @Test
+    public void testEquivalents()
+    {
+        Assert.assertEquals(m3_1, mbp.getEquivalentMatch(m1_3));
+        Assert.assertEquals(m4_1, mbp.getEquivalentMatch(m1_4));
+        Assert.assertEquals(m5server1_1, mbp.getEquivalentMatch(m1_5server1));
+        Assert.assertEquals(m4_1server3, mbp.getEquivalentMatch(m1server3_4));
+        Assert.assertEquals(null, mbp.getEquivalentMatch(m1server4_4));
+        Assert.assertEquals(null, mbp.getEquivalentMatch(m1server5_4));
+
+        Assert.assertEquals(null, mbp.getEquivalentMatch(m2_1));
+        Assert.assertEquals(null, mbp.getEquivalentMatch(m2_3));
+        Assert.assertEquals(m3server1_2, mbp.getEquivalentMatch(m2_3server1));
+
+        Assert.assertEquals(m1_3, mbp.getEquivalentMatch(m3_1));
+        Assert.assertEquals(m2_3server1, mbp.getEquivalentMatch(m3server1_2));
+
+        Assert.assertEquals(m1_4, mbp.getEquivalentMatch(m4_1));
+        Assert.assertEquals(m1server3_4, mbp.getEquivalentMatch(m4_1server3));
+        Assert.assertEquals(null, mbp.getEquivalentMatch(m4_2));
+
+        Assert.assertEquals(m1_5server1, mbp.getEquivalentMatch(m5server1_1));
+        Assert.assertEquals(null, mbp.getEquivalentMatch(m5server1_2));
+    }
+
+    private PatientMatch newMatch(String referencePatientId, String referenceServerId, String matchedPatientId, String matchedServerId)
+    {
+        PatientMatch m = mock(PatientMatch.class);
+        when(m.getReferencePatientId()).thenReturn(referencePatientId);
+        when(m.getReferenceServerId()).thenReturn(referenceServerId);
+        when(m.getMatchedPatientId()).thenReturn(matchedPatientId);
+        when(m.getMatchedServerId()).thenReturn(matchedServerId);
+        return m;
+    }
+}
