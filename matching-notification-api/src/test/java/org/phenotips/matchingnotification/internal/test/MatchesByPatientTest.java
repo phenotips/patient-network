be76a3874dcdff0d05027708c41af06d8962a4da
ItaiGershtansky <itaig.phenotips@gmail.com>
2016-07-20 17:39:54 -0400

    Removed dead code, Added id to mock PatientMatch-es.

--- matching-notification-api/src/test/java/org/phenotips/matchingnotification/internal/test/MatchesByPatientTest.java
+++ matching-notification-api/src/test/java/org/phenotips/matchingnotification/internal/test/MatchesByPatientTest.java
@@ -26,6 +26,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.google.common.base.Objects;
+
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -45,26 +47,26 @@ public class MatchesByPatientTest
     @Before
     public void setup()
     {
-        m1_3 = newMatch("P1", null, "P3", null);
-        m1_4 = newMatch("P1", null, "P4", null);
-        m1_5server1 = newMatch("P1", null, "P5", "server1");
-        m1server3_4 = newMatch("P1", "server3", "P4", null);
-        m1server4_4 = newMatch("P1", "server4", "P4", null);
-        m1server5_4 = newMatch("P1", "server5", "P4", null);
+        m1_3 = newMatch(1, "P1", null, "P3", null);
+        m1_4 = newMatch(2, "P1", null, "P4", null);
+        m1_5server1 = newMatch(3, "P1", null, "P5", "server1");
+        m1server3_4 = newMatch(4, "P1", "server3", "P4", null);
+        m1server4_4 = newMatch(5, "P1", "server4", "P4", null);
+        m1server5_4 = newMatch(6, "P1", "server5", "P4", null);
 
-        m2_1 = newMatch("P2", null, "P1", null);
-        m2_3 = newMatch("P2", null, "P3", null);
-        m2_3server1 = newMatch("P2", null, "P3", "server1");
+        m2_1 = newMatch(7, "P2", null, "P1", null);
+        m2_3 = newMatch(8, "P2", null, "P3", null);
+        m2_3server1 = newMatch(9, "P2", null, "P3", "server1");
 
-        m3_1 = newMatch("P3", null, "P1", null);
-        m3server1_2 = newMatch("P3", "server1", "P2", null);
+        m3_1 = newMatch(10, "P3", null, "P1", null);
+        m3server1_2 = newMatch(11, "P3", "server1", "P2", null);
 
-        m4_1 = newMatch("P4", null, "P1", null);
-        m4_1server3 = newMatch("P4", null, "P1", "server3");
-        m4_2 = newMatch("P4", null, "P2", null);
+        m4_1 = newMatch(12, "P4", null, "P1", null);
+        m4_1server3 = newMatch(13, "P4", null, "P1", "server3");
+        m4_2 = newMatch(14, "P4", null, "P2", null);
 
-        m5server1_1 = newMatch("P5", "server1", "P1", null);
-        m5server1_2 = newMatch("P5", "server1", "P2", null);
+        m5server1_1 = newMatch(15, "P5", "server1", "P1", null);
+        m5server1_2 = newMatch(16, "P5", "server1", "P2", null);
 
         this.mbp = new MatchesByPatient();
         mbp.add(m1_3);
@@ -153,44 +155,16 @@ public class MatchesByPatientTest
         Assert.assertTrue(matches.contains(m4_2));
     }
 
-    @Test
-    public void testEquivalents()
-    {
-        // Assert.assertEquals(m3_1, mbp.getEquivalentMatch(m1_3));
-        // Assert.assertEquals(m4_1, mbp.getEquivalentMatch(m1_4));
-        // Assert.assertEquals(m5server1_1,
-        // mbp.getEquivalentMatch(m1_5server1));
-        // Assert.assertEquals(m4_1server3,
-        // mbp.getEquivalentMatch(m1server3_4));
-        // Assert.assertEquals(null, mbp.getEquivalentMatch(m1server4_4));
-        // Assert.assertEquals(null, mbp.getEquivalentMatch(m1server5_4));
-        //
-        // Assert.assertEquals(null, mbp.getEquivalentMatch(m2_1));
-        // Assert.assertEquals(null, mbp.getEquivalentMatch(m2_3));
-        // Assert.assertEquals(m3server1_2,
-        // mbp.getEquivalentMatch(m2_3server1));
-        //
-        // Assert.assertEquals(m1_3, mbp.getEquivalentMatch(m3_1));
-        // Assert.assertEquals(m2_3server1,
-        // mbp.getEquivalentMatch(m3server1_2));
-        //
-        // Assert.assertEquals(m1_4, mbp.getEquivalentMatch(m4_1));
-        // Assert.assertEquals(m1server3_4,
-        // mbp.getEquivalentMatch(m4_1server3));
-        // Assert.assertEquals(null, mbp.getEquivalentMatch(m4_2));
-        //
-        // Assert.assertEquals(m1_5server1,
-        // mbp.getEquivalentMatch(m5server1_1));
-        // Assert.assertEquals(null, mbp.getEquivalentMatch(m5server1_2));
-    }
-
-    private PatientMatch newMatch(String referencePatientId, String referenceServerId, String matchedPatientId, String matchedServerId)
+    private PatientMatch newMatch(long id, String referencePatientId, String referenceServerId,
+        String matchedPatientId, String matchedServerId)
     {
         PatientMatch m = mock(PatientMatch.class);
+        when(m.getId()).thenReturn(id);
         when(m.getReferencePatientId()).thenReturn(referencePatientId);
         when(m.getReferenceServerId()).thenReturn(referenceServerId);
         when(m.getMatchedPatientId()).thenReturn(matchedPatientId);
         when(m.getMatchedServerId()).thenReturn(matchedServerId);
+        when(m.toString()).thenReturn(Objects.toStringHelper(m).add("id", id).toString());
         return m;
     }
 }
