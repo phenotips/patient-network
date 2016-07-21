2afec1447df40b202f671a964a7b2916e0874032
ItaiGershtansky <itaig.phenotips@gmail.com>
2016-07-21 17:39:21 -0400

    Added PatientInMatch.toJSON().

---
+++ matching-notification-api/src/main/java/org/phenotips/matchingnotification/match/internal/AbstractPatientInMatch.java
@@ -0,0 +1,60 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+package org.phenotips.matchingnotification.match.internal;
+
+import org.phenotips.components.ComponentManagerRegistry;
+import org.phenotips.matchingnotification.match.PatientInMatch;
+
+import org.xwiki.component.manager.ComponentLookupException;
+import org.xwiki.component.manager.ComponentManager;
+
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @version $Id$
+ */
+public abstract class AbstractPatientInMatch implements PatientInMatch
+{
+    private static final DefaultPatientMatchUtils UTILS;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractPatientInMatch.class);
+
+    static {
+        DefaultPatientMatchUtils utils = null;
+        try {
+            ComponentManager ccm = ComponentManagerRegistry.getContextComponentManager();
+            utils = ccm.getInstance(DefaultPatientMatchUtils.class);
+        } catch (ComponentLookupException e) {
+            LOGGER.error("Error loading static components: {}", e.getMessage(), e);
+        }
+        UTILS = utils;
+    }
+
+    @Override
+    public JSONObject toJSON() {
+        JSONObject json = new JSONObject();
+        json.put("patientId", this.getPatientId());
+        json.put("serverId", this.getServerId());
+        json.put("email", this.getEmail());
+        json.put("genes", UTILS.setToJSONArray(this.getCandidateGenes()));
+
+        return json;
+    }
+}
