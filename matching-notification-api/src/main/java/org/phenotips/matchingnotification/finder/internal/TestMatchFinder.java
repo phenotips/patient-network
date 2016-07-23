ddcabdcd19b1826adf8e58f72cebe780ef734ed9
itaig.phenotips@gmail.com <itaig.phenotips@gmail.com>
2016-07-23 12:08:03 -0400

    Set TestMatchFinder for testing matched/reference issues.

--- matching-notification-api/src/main/java/org/phenotips/matchingnotification/finder/internal/TestMatchFinder.java
+++ matching-notification-api/src/main/java/org/phenotips/matchingnotification/finder/internal/TestMatchFinder.java
@@ -17,12 +17,17 @@
  */
 package org.phenotips.matchingnotification.finder.internal;
 
+import org.phenotips.components.ComponentManagerRegistry;
 import org.phenotips.data.Patient;
 import org.phenotips.matchingnotification.finder.MatchFinder;
 import org.phenotips.matchingnotification.match.PatientMatch;
 import org.phenotips.matchingnotification.match.internal.DefaultPatientMatch;
+import org.phenotips.matchingnotification.match.internal.DefaultPatientMatchUtils;
+import org.phenotips.matchingnotification.match.internal.PhenotypesMap;
 
 import org.xwiki.component.annotation.Component;
+import org.xwiki.component.manager.ComponentLookupException;
+import org.xwiki.component.manager.ComponentManager;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -42,6 +47,27 @@ import org.slf4j.LoggerFactory;
 public class TestMatchFinder implements MatchFinder
 {
     private static final String SERVER1 = "SERVER1";
+    private static final String SERVER2 = "SERVER2";
+
+    private static final String Q1 = "Q0000001";
+    private static final String Q1_EMAIL = "mailto:q1@server1.com";
+    private static final String Q1_GENES = "q1_gene1;q1_gene2";
+
+    private static final String Q2 = "Q0000002";
+    private static final String Q2_EMAIL = "mailto:q2@server1.com";
+    private static final String Q2_GENES = "q2_gene1;q2_gene2";
+
+    private static final DefaultPatientMatchUtils UTILS;
+
+    static {
+        DefaultPatientMatchUtils utils = null;
+        try {
+            ComponentManager ccm = ComponentManagerRegistry.getContextComponentManager();
+            utils = ccm.getInstance(DefaultPatientMatchUtils.class);
+        } catch (ComponentLookupException e) {
+        }
+        UTILS = utils;
+    }
 
     /** */
     public class TestMatchData
@@ -68,6 +94,10 @@ public class TestMatchFinder implements MatchFinder
         public String matchedHref;
         /** */
         public String matchedGenes;
+        /** */
+        public String phenotypes;
+        /** */
+        public String matchedPhenotypes;
     }
 
     private Logger logger = LoggerFactory.getLogger(TestMatchFinder.class);
@@ -83,76 +113,59 @@ public class TestMatchFinder implements MatchFinder
         this.logger.debug("Finding test matches for patient {}.", patient.getId());
 
         List<PatientMatch> matches = new LinkedList<>();
-        matches.add(this.getMatch1(patient));
-        matches.add(this.getMatch2(patient));
-        matches.add(this.getMatch3(patient));
-        matches.add(this.getMatch4(patient));
+        if (patient.getId().equals("P0000001")) {
+            matches.add(this.getMatchedQ1(patient));
+            matches.add(this.getReferenceQ1(patient));
+        }
+        if (patient.getId().equals("P0000002")) {
+            matches.add(this.getMatchedQ1(patient));
+        }
+        if (patient.getId().equals("P0000003")) {
+            matches.add(this.getReferenceQ1(patient));
+        }
 
         return matches;
     }
 
-    private PatientMatch getMatch1(Patient patient) {
+    private PatientMatch getMatchedQ1(Patient patient) {
         TestMatchData td1 = new TestMatchData();
         td1.referencePatientId = patient.getId();
         td1.referenceServerId = null;
-        td1.matchedPatientId = "Q0000001";
+        td1.email = UTILS.getOwnerEmail(patient);
+        td1.genes = UTILS.setToString(UTILS.getGenes(patient));
+        td1.phenotypes = (new PhenotypesMap(patient)).toString();
+
+        td1.matchedPatientId = Q1;
         td1.matchedServerId = SERVER1;
+        td1.matchedHref = Q1_EMAIL;
+        td1.matchedGenes = Q1_GENES;
+        td1.matchedPhenotypes = (new PhenotypesMap()).toString();
+
         td1.score = 0.3;
         td1.genotypeScore = 0.0;
         td1.phenotypeScore = 0.6;
-        td1.email = "aaa@server.com";
-        td1.matchedHref = "mailto:matchedaaa@server.com";
-        td1.genes = "gene1;gene2";
-        td1.matchedGenes = "gene2;gene3";
+
         return new DefaultPatientMatch(td1);
     }
 
-    private PatientMatch getMatch2(Patient patient) {
-        TestMatchData td2 = new TestMatchData();
-        td2.referencePatientId = patient.getId();
-        td2.referenceServerId = null;
-        td2.matchedPatientId = "Q0000002";
-        td2.matchedServerId = SERVER1;
-        td2.score = 0.4;
-        td2.genotypeScore = 0.2;
-        td2.phenotypeScore = 0.6;
-        td2.email = "bbb@server.com";
-        td2.matchedHref = "https://decipher.org/patients/123412";
-        td2.genes = "gene1";
-        td2.matchedGenes = "gene1;gene3";
-        return new DefaultPatientMatch(td2);
-    }
+    private PatientMatch getReferenceQ1(Patient patient) {
+        TestMatchData td1 = new TestMatchData();
+        td1.matchedPatientId = patient.getId();
+        td1.matchedServerId = null;
+        td1.matchedHref = UTILS.getOwnerEmail(patient);
+        td1.matchedGenes = UTILS.setToString(UTILS.getGenes(patient));
+        td1.matchedPhenotypes = (new PhenotypesMap(patient)).toString();
 
-    private PatientMatch getMatch3(Patient patient) {
-        TestMatchData td3 = new TestMatchData();
-        td3.referencePatientId = patient.getId();
-        td3.referenceServerId = null;
-        td3.matchedPatientId = "Q0000009";
-        td3.matchedServerId = "server3";
-        td3.score = 0.1;
-        td3.genotypeScore = 0.0;
-        td3.phenotypeScore = 0.2;
-        td3.email = "ccc@server.com";
-        td3.matchedHref = "mailto:matchedccc@server.com";
-        td3.genes = "gene3";
-        td3.matchedGenes = "";
-        return new DefaultPatientMatch(td3);
-    }
+        td1.referencePatientId = Q1;
+        td1.referenceServerId = SERVER1;
+        td1.email = Q1_EMAIL;
+        td1.genes = Q1_GENES;
+        td1.phenotypes = (new PhenotypesMap()).toString();
 
-    private PatientMatch getMatch4(Patient patient) {
-        TestMatchData td4 = new TestMatchData();
-        td4.referencePatientId = patient.getId();
-        td4.referenceServerId = null;
-        td4.matchedPatientId = "Q0000010";
-        td4.matchedServerId = "server2";
-        td4.score = 0.7;
-        td4.genotypeScore = 0.8;
-        td4.phenotypeScore = 0.6;
-        td4.email = "ddd@server.com";
-        td4.matchedHref = "mailto:matchedddd@server.com";
-        td4.genes = "";
-        td4.matchedGenes = "";
-        return new DefaultPatientMatch(td4);
-    }
+        td1.score = 0.3;
+        td1.genotypeScore = 0.0;
+        td1.phenotypeScore = 0.6;
 
+        return new DefaultPatientMatch(td1);
+    }
 }
