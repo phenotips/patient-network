1a12c1b76134cae1ae21cb4e46f5a53f3db8dc78
Sergiu Dumitriu <sergiu@phenotips.org>
2017-03-27 14:43:12 -0400

    [misc] Use commons-collections4, cleaned up dependencies

--- matching-notification-api/src/main/java/org/phenotips/matchingnotification/script/MatchingNotificationScriptService.java
+++ matching-notification-api/src/main/java/org/phenotips/matchingnotification/script/MatchingNotificationScriptService.java
@@ -35,7 +35,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
