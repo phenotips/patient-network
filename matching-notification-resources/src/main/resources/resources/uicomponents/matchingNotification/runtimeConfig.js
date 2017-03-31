a7ac8c45ff6069dfcd044a0078a785af8ee15c7e
Sergiu Dumitriu <sergiu@phenotips.org>
2017-03-31 14:58:39 -0400

    PN-185: Matching notification tables sometimes fail to load Fixed

--- matching-notification-resources/src/main/resources/resources/uicomponents/matchingNotification/runtimeConfig.js
+++ matching-notification-resources/src/main/resources/resources/uicomponents/matchingNotification/runtimeConfig.js
@@ -2,5 +2,11 @@ require.config({
   paths : {
     matchingNotification : "$stringtool.substringBefore($xwiki.getSkinFile('uicomponents/matchingNotification/unnotifiedMatchesTable.js', true), 'unnotifiedMatchesTable.js')",
     dynatable: ["$!services.webjars.url('jquery-dynatable', 'jquery.dynatable.js')"]
+  },
+  shim : {
+    dynatable: {
+      deps: ['jquery'],
+      exports: 'dynatable'
+    }
   }
 });
