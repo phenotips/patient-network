3fe3045411a132e2a32fd308c374fd65a7c31c94
Andrew Misyura <asm@phenotips.org>
2018-03-09 17:17:40 -0500

    PN-275: Promp confirmation dialog when "For all patients" button is pushed on Administration's Refresh matches Done

--- matching-notification-resources/src/main/resources/resources/uicomponents/matchingNotification/findMatches.js
+++ matching-notification-resources/src/main/resources/resources/uicomponents/matchingNotification/findMatches.js
@@ -18,8 +18,76 @@ var PhenoTips = (function (PhenoTips) {
 
             this._utils = new utils();
 
+            this._dialog = this._createWarningDialogue();
+
             $('find-updated-matches-button').on('click', this._findUpdatedMatches.bind(this));
-            $('find-all-matches-button').on('click', this._findAllMatches.bind(this));
+            $('find-all-matches-button').on('click', this._showWarning.bind(this) );
+        },
+
+        _showWarning : function () {
+            var servers = this._getListOfServersToUse();
+            if (servers.length == 0) {
+                return;
+            }
+            var mmeIncluded = false;
+            servers.forEach(function(serverId) {
+                if (serverId != "local") {
+                    mmeIncluded = true;
+                }
+            });
+
+            if (mmeIncluded) {
+                this._warningBody.update("$services.localization.render('phenotips.findMatches.findAllWarningMME')");
+            }
+            else {
+                this._warningBody.update("$services.localization.render('phenotips.findMatches.findAllWarning')");
+            }
+
+            this._dialog.show();
+        },
+
+        _createWarningDialogue : function() {
+            var warningDiv = new Element('div', {'class': 'find-all-warning-body'});
+
+            this._warningBody = new Element('div', {'class': 'warning-text'});
+            warningDiv.insert(this._warningBody);
+
+            this._okButton = new Element('input', {type: 'button', name : 'ok',
+                'value': "$services.localization.render('phenotips.findMatches.findAllWarning.findAllButton')", 'class': 'button'});
+            this._cancelButton = new Element('input', {type: 'button', name : 'cancel',
+                'value': "$services.localization.render('phenotips.findMatches.findAllWarning.cancelButton')", 'class' : 'button secondary'});
+
+            var _this = this;
+            this._okButton.observe('click', function(event) {
+                _this._hideWarning();
+                _this._findAllMatches();
+            });
+
+            this._cancelButton.observe('click', function(event) {
+                _this._hideWarning();
+            });
+
+            var buttons = new Element('div', {'class' : 'buttons import-block-bottom'});
+            buttons.insert(this._okButton.wrap('span', {'class' : 'buttonwrapper'}))
+                   .insert(this._cancelButton.wrap('span', {'class' : 'buttonwrapper'}));
+            warningDiv.insert(buttons);
+
+            return new PhenoTips.widgets.ModalPopup(warningDiv, {close: {method: this._hideWarning.bind(this), keys: ['Esc']}},
+                              { extraClassName: "find-all-warning",
+                                title: "$services.localization.render('phenotips.findMatches.findAllWarningTitle')",
+                                displayCloseButton: false});
+        },
+
+        _hideWarning: function() {
+            this._dialog.closeDialog();
+        },
+
+        _getListOfServersToUse: function() {
+            var servers = [];
+            $$('.select-for-update input').each( function(elm) {
+                if (elm.checked) { servers.push(elm.value); }
+            } );
+            return servers;
         },
 
         _findUpdatedMatches : function() {
@@ -32,23 +100,18 @@ var PhenoTips = (function (PhenoTips) {
 
         _findMatches : function(onlyCheckPatientsUpdatedAfterLastRun, messageContainer)
         {
-            // disable all find matches buttons while matching is running...
-            $$('.find-matches-button').each( function(elm) { elm.disable() } );
-
-            var servers = [];
-            $$('.select-for-update input').each( function(elm) {
-                if (elm.checked) { servers.push(elm.value); }
-            } );
-
+            var servers = this._getListOfServersToUse();
             if (servers.length == 0) {
-                $$('.find-matches-button').each( function(elm) { elm.enable() } );
                 return;
             }
+
+            // disable all find matches buttons while matching is running...
+            $$('.find-matches-button').each( function(elm) { elm.disable() } );
 
             new Ajax.Request(this._ajaxURL, {
             	contentType:'application/json',
                 parameters : { "onlyCheckPatientsUpdatedAfterLastRun" : onlyCheckPatientsUpdatedAfterLastRun,
-                               "serverIds" : servers 
+                               "serverIds" : servers
                 },
                 onCreate : function() {
                     this._utils.showHint(messageContainer, "$services.localization.render('phenotips.matching.ajaxutils.requestSent')");
