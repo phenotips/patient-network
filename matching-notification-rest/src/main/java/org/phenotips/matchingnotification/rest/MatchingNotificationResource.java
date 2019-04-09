67e6d232d885d253f500ffada6e1ae65ba386f9b
Andrew Misyura <asm@phenotips.org>
2019-04-09 19:31:22 -0400

    PN-449: Remove the /get-matchid endpoint
    Removed the end-point and all related code. New MatchStorageManager API makes this endpoint redundant.
    Also added some timing code.

--- matching-notification-rest/src/main/java/org/phenotips/matchingnotification/rest/MatchingNotificationResource.java
+++ matching-notification-rest/src/main/java/org/phenotips/matchingnotification/rest/MatchingNotificationResource.java
@@ -177,25 +177,6 @@ public interface MatchingNotificationResource
     Response setStatus(@FormParam("matchesIds") Set<Long> matchesIds, @FormParam("status") String status);
 
     /**
-     * Returns a JSON object containing a single match ID found by 4 parameters: reference patient ID,
-     * matched patient ID, reference server ID, matched server ID.
-     *
-     * @param referencePatientId id of reference patient to load matches for
-     * @param referenceServerId id of the server that hosts reference patient
-     * @param matchedPatientId id of matched patient to load matches for
-     * @param matchedServerId id of the server that hosts matched patient
-     * @return a single matchID
-     */
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("/get-matchid")
-    Response getLastOutgoingMatchId(@FormParam("referencePatientId") String referencePatientId,
-        @FormParam("referenceServerId") String referenceServerId,
-        @FormParam("matchedPatientId") String matchedPatientId,
-        @FormParam("matchedServerId") String matchedServerId);
-
-    /**
      * Marks matches, with ids given in parameter, as user-contacted or not user-contacted. Example:
      *
      * <pre>
